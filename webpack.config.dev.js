0ea66f0950165b12aa3f1b3e52434ea554fdccc8
leolicona <leoncreactivo@gmail.com>
2021-12-04 23:32:27 -0600

    develop mode was created, optimization and cleanwebpack for production mode

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: "/",
+        filename: 'bundle.js'
     },
-    mode: 'production',
+    mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            'components': path.resolve(__dirname, './src/components/'),
-            'styles': path.resolve(__dirname, './src/styles/')
-
-        }
+        extensions: ['.js', '.jsx']
     },
     module: {
         rules: [
@@ -56,13 +47,12 @@ module.exports = {
         new MiniCssExtractPlugin(
             {filename: '[name].css'}
         ),
-        new CleanWebpackPlugin(),
       ],
-      optimization: {
-          minimize: true,
-          minimizer: [
-              new CssMinimizerPlugin(),
-              new TerserPlugin()
-          ]
-      }
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3006,
+      },
 }
